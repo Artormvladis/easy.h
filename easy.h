a514a78d1762351b29f7ce19fd0d6a197c568948
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-09-26 14:53:13 +0300

    Add files via upload

---
+++ easy.h
@@ -0,0 +1,19 @@
+#ifndef EASY_H_INCLUDED
+#define EASY_H_INCLUDED
+#include <iostream>
+void itc_name();
+void itc_fio();
+int itc_abs(int a1);
+double itc_fabs(double a1);
+int itc_revnbr(int a1);
+bool itc_iseven(int a1);
+int itc_max(int a1, int a2);
+int itc_min(int a1, int a2);
+double itc_fmax(double a1, double a2);
+double itc_fmin(double a1, double a2);
+int itc_sqrt(int a1);
+int itc_skv(int a1);
+int itc_spr(int a1, int a2);
+int itc_str(int a1, int a2, int a3);
+double itc_scir(int a1);
+#endif // EASY_H_INCLUDED
